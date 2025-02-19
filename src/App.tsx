c7c9a107b34c94ce6644982e6f0bd6c959bcf934
akshaya-biswal <mraxya@gmail.com>
2025-02-19 09:35:19 +0530

    Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import "./App.css";
-import Stopwatch from "./components/Stopwatch";
-// import User from "./components/User";
-// import Message from "./components/Message";
-// import { useState } from "react";
-// import Count from "./components/Count";
-// import DocumentTitle from "./components/DocumentTitle";
 
 const App = () => {
-  // const [data, setData] = useState({ name: "", age: 0 });
-
   return (
     <div className="App">
       <h1>App</h1>
-      <Stopwatch />
-      {/* <DocumentTitle /> */}
-      {/* <Message />
-      <User data={data} setData={setData} /> */}
     </div>
   );
 };
