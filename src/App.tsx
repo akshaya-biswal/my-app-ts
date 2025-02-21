8753d092cf2952b0901c50c074e74e782b3ae18a
akshaya-biswal <mraxya@gmail.com>
2025-02-21 09:43:06 +0530

    Clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import "./App.css";
-import Counter from "./components/Counter";
-import Parent from "./components/Parent";
-import { ThemeProvider } from "./components/ThemeContext";
-import ThemeSwitcher from "./components/ThemeSwitcher";
 
 const App = () => {
   return (
-    <ThemeProvider>
-      <div className="App">
-        <h1>App</h1>
-        <Counter />
-        <ThemeSwitcher />
-        <Parent />
-      </div>
-    </ThemeProvider>
+    <>
+      <div className="App"></div>
+    </>
   );
 };
 
